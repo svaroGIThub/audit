add7603976a6ef9dacd2d18f8912fe2f801bf914
Aldo Solano González <aldoglez34@gmail.com>
2019-07-26 17:54:41 -0500

    Config.json

--- models/user.js
+++ models/user.js
@@ -31,7 +31,7 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
-    lastName: {
+    phoneNumer: {
       type: DataTypes.STRING,
       allowNull: true
     }
