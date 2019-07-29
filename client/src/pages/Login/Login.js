35d03416f6b9fab72679dea7c5d9e0b445f3c3f4
Aldo Solano <aldoglez34@gmail.com>
2019-07-29 16:03:30 -0500

    msg

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -5,78 +5,88 @@ import API from "../../utils/API";
 import "./login.css";
 
 const styles = {
-    mainLogo: {
-        fontFamily: "Georgia",
-        fontWeight: 500,
-        marginBottom: 0,
-        color: "rgb(2, 29, 71)",
-        fontSize: "85px",
-        textAlign: "center"
-    },
-    slogan: {
-        fontFamily: "Georgia",
-        fontWeight: 300,
-        color: "rgb(2, 29, 71)",
-        fontSize: "18px",
-        textAlign: "center"
-    }
-}
+  mainLogo: {
+    fontFamily: "Georgia",
+    fontWeight: 500,
+    marginBottom: 0,
+    color: "rgb(2, 29, 71)",
+    fontSize: "85px",
+    textAlign: "center"
+  },
+  slogan: {
+    fontFamily: "Georgia",
+    fontWeight: 300,
+    color: "rgb(2, 29, 71)",
+    fontSize: "18px",
+    textAlign: "center"
+  }
+};
 
 class Login extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
 
-    state = {
-        email: "",
-        password: ""
-    };
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
-    handleInputChange = event => {
-        const { name, value } = event.target;
-        this.setState({ [name]: value });
-    };
+  handleLoginSubmit = event => {
+    event.preventDefault();
+    // alert(`email: ${this.state.email}\nPassword: ${this.state.password}`);
+    this.setState({ email: "", password: "" });
+    API.loginUser(this.state.email, this.state.password)
+      .then(res => window.location.replace(res.data))
+      .catch(err => console.log(err));
+  };
 
-    handleLoginSubmit = event => {
-        event.preventDefault();
-        // alert(`email: ${this.state.email}\nPassword: ${this.state.password}`);
-        this.setState({ email: "", password: "" });
-        API.loginUser(this.state.email, this.state.password)
-            .then(res => window.location.replace(res.data))
-            .catch(err => console.log(err));
-    };
-
-    render() {
-        return (
-            <Form className="form-signin">
-                <h1 style={styles.mainLogo}>G.A.A.</h1>
-                <p style={styles.slogan}>Governmental Audit Assistant</p>
-                <h4 className="mb-3 font-weight-normal text-dark text-center">Login</h4>
-                <Form.Group controlId="formBasicUser" className="mb-0">
-                    <Form.Control
-                        type="text"
-                        placeholder="Email"
-                        name="email"
-                        value={this.state.email}
-                        onChange={this.handleInputChange}
-                        required autoFocus />
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Control
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.handleInputChange} />
-                </Form.Group>
-                <Form.Group controlId="formBasicChecbox">
-                    <Form.Check type="checkbox" label="Remember me" className="text-muted" />
-                </Form.Group>
-                <Button
-                    className="btn-lg"
-                    variant="primary"
-                    type="submit"
-                    block onClick={this.handleLoginSubmit}>Enter</Button>
-            </Form >
-        );
-    }
+  render() {
+    return (
+      <Form className="form-signin">
+        <h1 style={styles.mainLogo}>G.A.A.</h1>
+        <p style={styles.slogan}>Governmental Audit Assistant</p>
+        <h4 className="mb-3 font-weight-normal text-dark text-center">Login</h4>
+        <Form.Group controlId="formBasicUser" className="mb-0">
+          <Form.Control
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleInputChange}
+            required
+            autoFocus
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicChecbox">
+          <Form.Check
+            type="checkbox"
+            label="Remember me"
+            className="text-muted"
+          />
+        </Form.Group>
+        <Button
+          className="btn-lg"
+          variant="primary"
+          type="submit"
+          block
+          onClick={this.handleLoginSubmit}
+        >
+          Enter
+        </Button>
+      </Form>
+    );
+  }
 }
 
 export default Login;
